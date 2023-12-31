c7ebb10d77e3a4920f3efb3a16c3d640fbb440b7
Jacob <angell.zhfg@gmail.com>
2023-12-31 12:49:23 +0800

    update

--- dags/eastmonery/utils/_requests.py
+++ dags/eastmonery/utils/_requests.py
@@ -1,4 +1,5 @@
 from requests import Session, cookies
+import requests
 
 def create_session():
     headers  = {
@@ -26,7 +27,7 @@ def create_session():
     session.headers = headers
     session.cookies = cookies_jar
 
-    rsp = session.get("http://47.push2.eastmoney.com/api/qt/clist/get?pn=1&pz=1000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=|0|0|0|web&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1701597491665")
+    rsp = requests.get("http://47.push2.eastmoney.com/api/qt/clist/get?pn=1&pz=1000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=|0|0|0|web&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1701597491665")
     print("======================{}=====================".format(rsp.text))
     return session
 
