c6262cb5cbd09d98d4ccbe2b71a61f95b66d4c4c
Jacob <angell.zhfg@gmail.com>
2023-12-31 12:52:51 +0800

    update

--- dags/eastmonery/stock.py
+++ dags/eastmonery/stock.py
@@ -1,10 +1,10 @@
-from eastmonery.utils._requests import create_session
+# from eastmonery.utils._requests import create_session
 from datetime import datetime
 import subprocess
 import json
 import requests
 
-session = create_session()
+# session = create_session()
 
 def get_all_a_stock():
     all_stock_list = []
@@ -31,9 +31,8 @@ def get_all_a_stock():
 
         while not is_last_page:
             url = config.get("url_pattern").format(pn = pn, pz = pz)
-            print(url)
             pn += 1
-            rsp = session.get(url)
+            rsp = requests.get(url)
             if rsp.status_code == 200:
                 rsp_json_data_diff = json.loads(rsp.text).get("data").get("diff")
 
@@ -70,7 +69,7 @@ def get_stock_detail(market, code, end_date=int(datetime.timestamp(datetime.now(
     detail_url = "https://push2.eastmoney.com/api/qt/stock/get?invt=2&fltt=1&fields=f58%2Cf734%2Cf107%2Cf57%2Cf43%2Cf59%2Cf169%2Cf301%2Cf60%2Cf170%2Cf152%2Cf177%2Cf111%2Cf46%2Cf44%2Cf45%2Cf47%2Cf260%2Cf48%2Cf261%2Cf279%2Cf277%2Cf278%2Cf288%2Cf19%2Cf17%2Cf531%2Cf15%2Cf13%2Cf11%2Cf20%2Cf18%2Cf16%2Cf14%2Cf12%2Cf39%2Cf37%2Cf35%2Cf33%2Cf31%2Cf40%2Cf38%2Cf36%2Cf34%2Cf32%2Cf211%2Cf212%2Cf213%2Cf214%2Cf215%2Cf210%2Cf209%2Cf208%2Cf207%2Cf206%2Cf161%2Cf49%2Cf171%2Cf50%2Cf86%2Cf84%2Cf85%2Cf168%2Cf108%2Cf116%2Cf167%2Cf164%2Cf162%2Cf163%2Cf92%2Cf71%2Cf117%2Cf292%2Cf51%2Cf52%2Cf191%2Cf192%2Cf262%2Cf294%2Cf295%2Cf269%2Cf270%2Cf256%2Cf257%2Cf285%2Cf286%2Cf748%2Cf747&secid={market}.{code}&ut=fa5fd1943c7b386f172d6893dbfba10b&wbp2u=%7C0%7C0%7C0%7Cweb&_={end_date}"
     url = detail_url.format(market=market, code=code, end_date=end_date)
     data = json.loads(
-        session.get(url).text
+        requests.get(url).text
     )
 
     return data
@@ -97,7 +96,7 @@ def get_kline(market, code, klt=101, fq=0, pg_size=3000, end_data=int(datetime.t
     url = kline_url.format(market=market, code=code, klt=101, fq=0, pg_size=3000, end_date=int(datetime.timestamp(datetime.now())*1000))
 
     data = json.loads(
-        session.get(url).text
+        requests.get(url).text
     )
 
     return data
