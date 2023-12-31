bf8ab61cee17d5fbab4c1f782a08bd26be1b5791
Jacob <angell.zhfg@gmail.com>
2023-12-31 22:01:20 +0800

    upate

--- dags/eastmonery/test.py
+++ dags/eastmonery/test.py
@@ -22,22 +22,22 @@ adapter = HTTPAdapter(max_retries=retry_strategy)
 
 def create_session():
     headers  = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "Host": "stock.eastmoney.com",
-        "Pragma": "no-cache",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
+        # "Accept-Language": "zh-CN,zh;q=0.9",
+        # "Cache-Control": "no-cache",
+        # "Connection": "keep-alive",
+        # "Host": "stock.eastmoney.com",
+        # "Pragma": "no-cache",
+        # "Sec-Fetch-Dest": "document",
+        # "Sec-Fetch-Mode": "navigate",
+        # "Sec-Fetch-Site": "none",
+        # "Sec-Fetch-User": "?1",
+        # "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
-        "sec-ch-ua": 'Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "Windows",
+        # "sec-ch-ua": 'Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+        # "sec-ch-ua-mobile": "?0",
+        # "sec-ch-ua-platform": "Windows",
     }
 
     # get eastmonery url first to get headers and cookies
