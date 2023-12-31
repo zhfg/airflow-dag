193057671aaf2fcffb11e947e6835e6cb57b5fa8
Jacob <angell.zhfg@gmail.com>
2023-12-31 20:04:44 +0800

    update

--- dags/eastmonery/stock_list_to_minio.py
+++ dags/eastmonery/stock_list_to_minio.py
@@ -2,6 +2,26 @@ from airflow import DAG, task
 from datetime import datetime
 from airflow.operators.python import PythonVirtualenvOperator
 from pendulum import datetime, duration
+import sys
+sys.path.append("/opt/bitnami/airflow/dags/git_airflow-dag/dags")
+from eastmonery.minio import (
+    create_minio_client, 
+    minio_update_file, 
+    minio_upload_stock_list,
+    minio_get_stock_list,
+    minio_upload_daily_kline,
+    )
+
+bucket = "stock"
+minio_endpoint = "192.168.1.151:9003"
+access_key = "Eecd8UOBiMxiVGnPHXcq"
+secret_key = "Ap2j4yY7aJ2bq870f6xuYp5axI66ZXcBKb6CeKwb"
+
+minio_client = create_minio_client(
+    endpoint=minio_endpoint,
+    access_key=access_key,
+    secret_key=secret_key
+)
 
 dag_args = {
     "retries": 5,
@@ -104,10 +124,11 @@ with DAG(
     )
     task_2_group = []
     stocks = minio_get_stock_list(minio_client, bucket)
-    task_2 = PythonVirtualenvOperator(
-        task_id = "daily_kline_from_east_monery",
-        requirements=requirements,
-        python_callable=daily_kline_from_east_monery,
+    task_2_group.append(
+        PythonVirtualenvOperator(
+            task_id = "daily_kline_from_east_monery",
+            requirements=requirements,
+            python_callable=daily_kline_from_east_monery,
+        )
     )
-
-    task_1 >> task_2
+    task_1 >> task_2_group
