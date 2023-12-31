467a83be83955fda841514ac5a3ab1e127087da1
Jacob <angell.zhfg@gmail.com>
2023-12-31 18:21:54 +0800

    update

--- dags/eastmonery/stock_list_to_minio.py
+++ dags/eastmonery/stock_list_to_minio.py
@@ -70,6 +70,7 @@ with DAG(
         from concurrent.futures import ThreadPoolExecutor, as_completed
         
         stocks = minio_get_stock_list(minio_client, bucket)
+        print(stocks)
         for stock in stocks:
             print(stock, type(stock))
             # stock = json.loads(stock)
