39390573767835ce4ab6a051046a1d21aa4901cb
Jacob <angell.zhfg@gmail.com>
2024-01-11 21:24:49 +0800

    update

--- dags/eastmonery/stock_list_to_minio.py
+++ dags/eastmonery/stock_list_to_minio.py
@@ -5,7 +5,7 @@ from pendulum import datetime, duration
 import sys
 sys.path.append("/opt/bitnami/airflow/dags/git_airflow-dag/dags")
 
-from eastmonery.stock import stock_from_east_monery
+from dags.eastmonery.utils.stock import stock_from_east_monery
 
 dag_args = {
     "retries": 1,
@@ -64,14 +64,14 @@ with DAG(
         secret_key = "Ap2j4yY7aJ2bq870f6xuYp5axI66ZXcBKb6CeKwb"
 
         sys.path.append("/opt/bitnami/airflow/dags/git_airflow-dag/dags")
-        from eastmonery.minio import (
+        from eastmonery.utils._minio import (
             create_minio_client, 
             minio_update_file, 
             minio_upload_stock_list,
             minio_get_stock_list,
             minio_upload_daily_kline,
             )
-        from eastmonery.stock import get_all_a_stock, get_kline, get_stock_detail
+        from eastmonery.utils.stock import get_all_a_stock, get_kline, get_stock_detail
         minio_client = create_minio_client(
             endpoint=minio_endpoint,
             access_key=access_key,
@@ -107,7 +107,7 @@ with DAG(
         secret_key = "Ap2j4yY7aJ2bq870f6xuYp5axI66ZXcBKb6CeKwb"
 
         sys.path.append("/opt/bitnami/airflow/dags/git_airflow-dag/dags")
-        from eastmonery.minio import (
+        from eastmonery.utils._minio import (
             create_minio_client, 
             minio_update_file, 
             minio_upload_stock_list,
@@ -115,13 +115,13 @@ with DAG(
             minio_upload_daily_kline,
             minio_get_stock_kline,
             )
-        from eastmonery.stock import get_all_a_stock, get_kline, get_stock_detail
+        from eastmonery.utils.stock import get_all_a_stock, get_kline, get_stock_detail
         minio_client = create_minio_client(
             endpoint=minio_endpoint,
             access_key=access_key,
             secret_key=secret_key
         )
-        from eastmonery.analisys import count_continual_limit_up, find_highst_close_index
+        from eastmonery.utils.analisys import count_continual_limit_up, find_highst_close_index
         from concurrent.futures import ThreadPoolExecutor, as_completed
         
         stocks = minio_get_stock_list(client=minio_client, bucket=bucket, )
--- dags/eastmonery/utils/stock.py
+++ dags/eastmonery/utils/stock.py
@@ -6,8 +6,8 @@ from time import sleep, ctime
 import sys, os, time, json, io
 
 sys.path.append("/opt/bitnami/airflow/dags/git_airflow-dag/dags")
-from eastmonery.minio import create_minio_client, minio_update_file, minio_upload_stock_list, minio_get_stock_list
-from eastmonery.stock import get_all_a_stock, get_kline, get_stock_detail
+from eastmonery.utils._minio import create_minio_client, minio_update_file, minio_upload_stock_list, minio_get_stock_list
+from eastmonery.utils.stock import get_all_a_stock, get_kline, get_stock_detail
 
 session = create_session()
 
