078c736da360f34cf249c3253d8c49fd590723e8
Jacob <angell.zhfg@gmail.com>
2024-01-13 08:33:12 +0800

    update

--- dags/eastmonery/stock_list_to_minio.py
+++ dags/eastmonery/stock_list_to_minio.py
@@ -57,61 +57,17 @@ with DAG(
             secret_key,
         )  
         
-
-    def find_want_stocks():
-        from threading import Thread
-        from time import sleep, ctime
-        import sys, os, time, json, io
-        bucket = "stock"
-        minio_endpoint = "192.168.1.151:9003"
-        access_key = "Eecd8UOBiMxiVGnPHXcq"
-        secret_key = "Ap2j4yY7aJ2bq870f6xuYp5axI66ZXcBKb6CeKwb"
-
+    def task_find_want_stocks():
+        import sys
         sys.path.append("/opt/bitnami/airflow/dags/git_airflow-dag/dags")
-        from eastmonery.utils._minio import (
-            create_minio_client, 
-            minio_update_file, 
-            minio_upload_stock_list,
-            minio_get_stock_list,
-            minio_upload_daily_kline,
-            minio_get_stock_kline,
-            )
-        from eastmonery.utils.stock import get_all_a_stock, get_kline, get_stock_detail
-        minio_client = create_minio_client(
-            endpoint=minio_endpoint,
-            access_key=access_key,
-            secret_key=secret_key
-        )
-        from eastmonery.utils.analisys import count_continual_limit_up, find_highst_close_index
-        from concurrent.futures import ThreadPoolExecutor, as_completed
-        
-        stocks = minio_get_stock_list(client=minio_client, bucket=bucket, )
-        satisfied_stocks: list = []
-        for stock in stocks.get("all_stocks"):
-            name = stock.get("name")
-            market=stock.get("market")
-            code = stock.get("code")
-            day_kline = minio_get_stock_kline(client=minio_client, bucket=bucket, market=market, code=code)
-            # 统计最后10个交易日的连续涨停
-            dktotal = day_kline.get("data").get("dktotal")
-            last_10_datas = day_kline.get("data").get("klines")[-30:]
-            last_10_datas = [x.split(',') for x in last_10_datas]
-            b, i, c = count_continual_limit_up(name, market, code, last_10_datas)
 
-            ## 过滤连续2个或3个涨停的股票
-            if c > 2 or c > 3:
-                #  过去10个交易日最高价出现在这两个交易日上
-                ## 找到最高收盘价所在的位置, 对比其所在的数据索引与连续涨停的位，判断其是否与最后一个涨停所在的位置是否一致
-                highest_close_index, highest_close = find_highst_close_index(last_10_datas)
-                if highest_close_index == i or highest_close_index == i + 1:
-                    close_rate = float(last_10_datas[-1][2]) / highest_close  
-                    print("name: {}, market: {}, code: {}, 最大连续涨停次数: {}, 最高价出现的位置: {}, 最高价格: {}, 连椟涨停最后一个位置: {}, 收盘价与最高价的比值: {}, 今日收盘: {}, 10日内最高价: {}".format(name, market, code, c, highest_close_index, highest_close, i, close_rate, last_10_datas[-1][2], highest_close))           
-                    # if close_rate > 0.41 and close_rate < 0.51:
-                    #     print("symbol: {}, 最大连续涨停次数: {}, 最高价出现的位置: {}, 最高价格: {}, 连椟涨停最后一个位置: {}, 收盘价与最高价的比值: {}, 今日收盘: {}, 10日内最高价: {}".format(stock.symbol, c, highest_close_index, highest_close, i, close_rate, last_10_datas[-1].close, highest_close))
-                    #     satisfied_stocks.append(stock)  
-                # 今日收盘价与最高价的比值在41%到51%之间的股票
-                
-        return satisfied_stocks
+        from eastmonery.utils.analisys import find_want_stocks
+        find_want_stocks(
+            bucket,
+            minio_endpoint,
+            access_key,
+            secret_key,
+        )
 
     task_1 = ExternalPythonOperator(
         task_id="stock_from_east_monery",
@@ -122,7 +78,7 @@ with DAG(
             minio_endpoint,
             access_key,
             secret_key,
-            ),
+        ),
     )
 
     task_2 = ExternalPythonOperator(
@@ -133,15 +89,21 @@ with DAG(
             minio_endpoint,
             access_key,
             secret_key,
-            ),
+        ),
         python_callable=task_daily_kline_from_east_monery,
     )
 
-    # task_3 = PythonVirtualenvOperator(
-    #     task_id = "find_wiat_stocks",
-    #     requirements=requirements,
-    #     python_callable=find_want_stocks,
-    # )
+    task_3 = ExternalPythonOperator(
+        task_id = "find_wiat_stocks",
+        python="/venvs/airflow/bin/python",
+        op_args=(
+            bucket,
+            minio_endpoint,
+            access_key,
+            secret_key,
+        ),
+        python_callable=task_find_want_stocks,
+    )
 
-    task_1 >> task_2
+    task_1 >> task_2 >> task_3
     # task_3
--- dags/eastmonery/utils/analisys.py
+++ dags/eastmonery/utils/analisys.py
@@ -1,74 +1,156 @@
+import sys
+sys.path.append("/opt/bitnami/airflow/dags/git_airflow-dag/dags")
+from eastmonery.utils._requests import create_session
+from eastmonery.utils._minio import create_minio_client, minio_update_file, minio_upload_stock_list, minio_get_stock_list, minio_get_stock_kline
+import pandas as pd
 
+def append_kdj(df):
+    # 使用参数： 9,3,3
+    # 9天最低价
+    lowest = df['lower'].rolling(9).min()
+    lowest.fillna(value=df['lower'].expanding().min(), inplace = True)
 
-def is_limit_up(market: int, code: str, data: list):
-    # data
-    # 日期，今开，今收，最新价，最低价，成交价，成交量，振幅，涨跌幅， 涨跌额，换手率
-    # print(symbol, data.timestamp, data.percent)
+    # 9天最高价
+    highest = df['high'].rolling(9).max()
+    highest.fillna(value=df['high'].expanding().max(), inplace = True)
+
+    # 计算RSV
+    rsv = (df.close-lowest) / (highest - lowest) * 100
+
+    # 前面9天是nan, 填充为 100.0
+    rsv.fillna(value=100.0, inplace=True)
+
+    df['k'] = rsv.ewm(com=2, adjust=False).mean()
+    df['d'] = df['k'].ewm(com=2, adjust=False).mean()
+    df['j'] = 3 * df['k'] - 2 * df['d']
+
+def find_prevoid_highest_close(datas, days: int):
+    datas = datas.tail(days)
+    highest_close = datas.max()
+    highest_close_index = datas.idxmax()
+    return highest_close, highest_close_index
+
+def find_prevoid_lowest_close(datas, days: int):
+    datas = datas.tail(days)
+    lowest_close = datas.min()
+    lowest_close_index = datas.idxmin()
+    return lowest_close, lowest_close_index
+
+def count_continuous_limit_up(code, datas, limit_up_rate):
+    counts = []
+    count = 0
+    is_first= True
+    for chg in datas:
+        if chg >= limit_up_rate:
+            if is_first:
+                counts.append(count)
+                count = 1
+            else:
+                count += 1
+            is_first = False
+        else:
+            if not is_first and count != 0:
+                counts.append(count)
+            is_first = True
+            count = 0
+            
+    # 当最后一天为涨停时，循环结束后，加入统计
+    if count != 0:
+        counts.append(count) 
+    return counts
+
+def count_down_with_rate(code, datas, down_rate):
+    count = 0
+    is_first= True
+    for chg in datas:
+        if chg <= down_rate:
+            count += 1
+            
+    return count
+
+def get_up_limit_rate(market, code):
     if market==1:
         if code.startswith("688"):
-            if float(data[8]) >= 19.95:
-                return True
-            else:
-                return False
+            return 19.95
         else:
-            if float(data[8]) >= 9.95:
-                return True
-            else:
-                return False            
+            return 9.95           
     if market==0:
-        if code.startswith("300"):
-            if float(data[8]) >= 19.95:
-                return True
-            else:
-                return False
+        if code.startswith("30"):
+            return 19.95
         elif code.startswith("00"):
-            if float(data[8]) >= 9.95:
-                return True
-            else:
-                return False 
-
-def count_continual_limit_up(name, market, code, datas):
-    # 统计一个k线序列的涨停数据
-
-    # 返回：统计一个K线序列中最后一个连续涨停数据，最少一个涨停
-    # 返回：是否有涨停，连续涨中的第一个涨停发生在第几个数据，一共连续多少天涨停
-    # 返回：False/True, index, count
-
-    # 数据序列的长度少于10直接返回
-    if len(datas) < 30:
-        return False, 0, 0
-    last_10_high: float = 0.00  # 10日内最高价
-    current_close: float = 0.00 # 当日收盘价
-    count_limit_up: int = 0     # 连续张停计数
-    is_last_day_limit_up: bool = False #标志昨日是否涨停
-    last_day_limit_up_index: int = 0  #标记最后连续涨停位置
-    is_first_limit_up: bool = False # 标记是否第一个涨停位
-    index: int = 0
-    for data in datas:
-        if is_limit_up(market, code, data):
-            if is_last_day_limit_up:
-                count_limit_up += 1
-            else:
-                count_limit_up = 1
-            is_last_day_limit_up = True
-            last_day_limit_up_index = index
-        else: 
-           is_last_day_limit_up = False 
-        index += 1
-    if count_limit_up > 0:
-        return True, last_day_limit_up_index, count_limit_up
-    else:
-        return False, 0, 0
-    
-def find_highst_close_index(datas: list):
-    highest_close: float = 0.00
-    highest_close_index: int = 0
-    index: int = 0
-    for d in datas:
-        current_close: float = float(d[2])
-        if current_close > highest_close:
-            highest_close = current_close
-            highest_close_index = index
-        index += 1
+            return 9.95
         
-    return highest_close_index, highest_close
+def find_want_stocks(bucket,minio_endpoint, minio_access_key,minio_secret_key):
+    minio_client = create_minio_client(
+        endpoint=minio_endpoint,
+        access_key=minio_access_key,
+        secret_key=minio_secret_key
+    )
+    stocks = minio_get_stock_list(minio_client, bucket)
+    print("股票名称\t市场\t股票代码\t近期最高价\t近期最高价出现位置\t近期最低价\t近期最低价的位置")
+    for stock in stocks.get("all_stocks"):
+        i += 1
+        name = stock.get("name")
+        market=stock.get("market")
+        code = stock.get("code")
+        day_kline = minio_get_stock_kline(client=client, bucket=bucket, market=market, code=code)
+        klines = day_kline.get("data").get("klines")
+        kline_len = len(klines)
+        datas = []
+        for k in klines:
+            k = k.split(",")
+            k_time = k[0]
+            k_open = float(k[1])
+            k_close = float(k[2])
+            k_high = float(k[3])
+            k_lower = float(k[4])
+            k_volume = float(k[5])
+            k_amount = float(k[6])
+            k_chg = float(k[8])
+            datas.append([k_time, k_open, k_close, k_high, k_lower, k_volume, k_amount, k_chg])
+            
+        columes = ['time', 'open', 'close', 'high', 'lower', 'volume', 'amount', 'chg']
+        df = pd.DataFrame(datas, columns=columes,)
+
+        if len(df) < 200:
+            continue
+        append_kdj(df)
+        # 找到最高价和近期最低价
+        high_close, high_index = find_prevoid_highest_close(df['high'], 30)
+        lowest_close, lowest_index = find_prevoid_lowest_close(df['close'], 5)
+
+        if lowest_index >= high_index and lowest_index < kline_len:
+            if high_close/lowest_close > 1.46 or high_close/lowest_close < 1.51:
+                # 统计最后价前十个交易日的连续涨停情况
+                up_limit_rate = get_up_limit_rate(market, code)
+                counts = count_continuous_limit_up(code, df['chg'].head(high_index+1).tail(10), up_limit_rate)
+                match = False
+                for count in counts:
+                    if count > 3:
+                        match = True
+                if match:
+                    max_continuous_up_limit = 0
+                    for limit in counts:
+                        if max_continuous_up_limit < limit:
+                            max_continuous_up_limit = limit
+                    
+                    # 剔除出现最高价后有大阴线的股票
+                    # 1. 统计每天跌幅大于跌停70%的股票,删除天数大于前面统计的连续涨停中最大连续涨停的天数
+                    # 2. 删除最高价出现那天以来，到今天不要5天的股票
+                    # 3. 统计自最高价出现那天以来，日跌幅大于跌停70%的天数， 删除总天数超过自最高价出现以来到今天的交易总天数的1/5的股票
+                    exclude_down_rate = (0-up_limit_rate) * 0.7
+                    after_limit_up_days = kline_len - high_index
+                    # if code == "002888":
+                    #     print(after_limit_up_days)
+                    if after_limit_up_days < 5:
+                        continue
+                    down_count = count_down_with_rate(code, df['chg'].tail(after_limit_up_days), exclude_down_rate)
+                    down_match = False
+
+                    
+                    if down_count > max_continuous_up_limit and down_count/after_limit_up_days > 0.2:
+                        down_match = True
+                    if not down_match:
+                        print("{}\t{}\t{}\t{}\t\t{}\t\t\t{}\t\t{}\t\t{}\t\t\t\t\t{}".format(
+                            name, market, code, high_close, high_index, lowest_close, lowest_index, counts, up_limit_rate
+                        ))
